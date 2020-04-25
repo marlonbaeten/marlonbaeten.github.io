0b8421b890dfff55a1fe91cfec7aced6c6e960fd
Marlon Baeten <marlon@baeten.org>
2020-04-25 21:44:52 +0200

    Color animation

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -5,26 +5,10 @@ const pad = (num, count = 2) => String(num).padStart(count, '0');
 
 export default function Countdown({ running, duration }) {
   const minutes = pad(Math.floor(duration / 60));
-  const [state, setState] = useState(99);
-  const startTime = useRef(0);
-  const requestRef = useRef(null);
-
-  React.useEffect(() => {
-    const animate = () => {
-      if (running) {
-        setState(Math.round(99 - ((new Date()).getTime() - startTime.current) / 10));
-      }
-      requestRef.current = requestAnimationFrame(animate);
-    }
-
-    startTime.current = (new Date()).getTime();
-    requestRef.current = requestAnimationFrame(animate);
-    return () => cancelAnimationFrame(requestRef.current);
-  }, [duration, running]);
 
   return (
-    <div className="config">
-      {minutes}:{pad(duration % 60)}:{pad(state)}
+    <div className="countdown">
+      {minutes}:{pad(duration % 60)}
     </div>
   );
 }
