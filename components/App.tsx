ddf341373302b41e138a6398707dc87e4c775d70
Marlon Baeten <marlon@baeten.org>
2020-04-27 19:58:47 +0200

    Added Readme

--- components/App.tsx
+++ components/App.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../style.scss';
 import useTimer, { Mode } from './useTimer';
 import Config from './Config';
@@ -10,39 +10,54 @@ const App = () => {
   const { state, dispatch } = useTimer();
   const action = state.actions[state.actionCounter];
 
+  const [style, setStyle] = useState<object>({});
+
+  useEffect(() => {
+    if (state.running && !state.done && state.actions[state.actionCounter].mode === Mode.ACTIVE) {
+      setStyle({
+        animation: 'colorchange',
+        animationDuration: `${state.actions[state.actionCounter].duration}s`,
+      });
+    } else {
+      setStyle({});
+    }
+  }, [state.actionCounter]);
+
   return (
-    <div className="container">
-      <h1>Robo Mark</h1>
-      {(state.running || !state.done) ? (
-        <div className={`timer ${action.mode === Mode.ACTIVE && 'active'} ${action.mode === Mode.REST && 'rest'} ${action.mode === Mode.PASSIVE && 'passive'}`}>
-          <div className="action">
-            {action.mode === Mode.ACTIVE && 'Work!'}
-            {action.mode === Mode.REST && '... Rest ..'}
-            {action.mode === Mode.PASSIVE && 'Chill'}
-          </div>
-          <div className="time">
-            <span className="label">Time</span>
-            <Countdown duration={state.actionTime} running={state.running} />
-          </div>
-          {action.mode !== Mode.REST && (
-            <div className="set">
-              <span className="label">Set</span>
-              <span>{action.set}/{state.sets}</span>
+    <div className="page" style={style}>
+      <div className="container">
+        <h1>Robo Mark 0.1</h1>
+        {(state.running || !state.done) ? (
+          <div className={`timer ${action.mode === Mode.ACTIVE && 'active'} ${action.mode === Mode.REST && 'rest'} ${action.mode === Mode.PASSIVE && 'passive'}`}>
+            <div className="action">
+              {action.mode === Mode.ACTIVE && 'Work!'}
+              {action.mode === Mode.REST && '... Rest ..'}
+              {action.mode === Mode.PASSIVE && 'Chill'}
             </div>
-          )}
-          <div className="round">
-            <span className="label">Round</span>
-            <span>{action.round}/{state.rounds}</span>
+            <div className="time">
+              <span className="label">Time</span>
+              <Countdown duration={state.actionTime} running={state.running} />
+            </div>
+            {action.mode !== Mode.REST && (
+              <div className="set">
+                <span className="label">Set</span>
+                <span>{action.set}/{state.sets}</span>
+              </div>
+            )}
+            <div className="round">
+              <span className="label">Round</span>
+              <span>{action.round}/{state.rounds}</span>
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className="not-running">
-          <Config state={state} dispatch={dispatch} />
-          {action && (<h2>Done!</h2>)}
-          {!action && (<h2>Ready?!</h2>)}
-        </div>
-      )}
-      <Actions state={state} dispatch={dispatch} />
+        ) : (
+          <div className="not-running">
+            <Config state={state} dispatch={dispatch} />
+            {action && (<h2>Done!</h2>)}
+            {!action && (<h2>Ready?!</h2>)}
+          </div>
+        )}
+        <Actions state={state} dispatch={dispatch} />
+      </div>
     </div>
   );
 };
